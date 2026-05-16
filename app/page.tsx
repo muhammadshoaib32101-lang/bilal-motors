816c1a83d50da0c432fa05c022c762bed9606a90
muhammadshoaib32101-lang <muhammadshoaib32101@gmail.com>
2026-05-16 14:49:30 +0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-﻿// app/page.tsx
-import { redirect } from "next/navigation";
-
-export default function RootPage() {
-  // This ensures that anyone hitting "/" is sent to the auth page
-  redirect("/auth/login");
-}
